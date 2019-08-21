bf7dee0321ac7a512655b0450903a6ad284f86f0
assada <assada.ua@gmail.com>
2019-08-21 23:54:49 +0300

    Caddy

--- app/routes/web.php
+++ app/routes/web.php
@@ -10,7 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-\Illuminate\Support\Facades\URL::forceRootUrl(env('APP_URL'));
+
+$url = parse_url(env('APP_URL'));
+\Illuminate\Support\Facades\URL::forceRootUrl($url['host']);
+\Illuminate\Support\Facades\URL::forceScheme($url['scheme']);
+
 Route::get('/', ['uses' => 'Index@index']);
 Route::get('/add', ['uses' => 'Index@add']);
 Route::post('/add', ['uses' => 'Index@store'])->name('store');
